0d20ee318019ba17fdb5f50ed87b137e151f1e6c
Mayank Mittal <mayank.mittal@allocatesoftware.com>
2020-09-12 00:13:20 +0530

    Updates

---
+++ precache-manifest.95f23962c692577f28de3354db7e731c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0497046d70f985c921e6172498bfbc8e",
+    "url": "/MakeTheBiggerSum/index.html"
+  },
+  {
+    "revision": "0cad7527d1f2a79b466c",
+    "url": "/MakeTheBiggerSum/static/js/2.e35a6dff.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/MakeTheBiggerSum/static/js/2.e35a6dff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c4144ced1108ab063882",
+    "url": "/MakeTheBiggerSum/static/js/main.e3b6c813.chunk.js"
+  },
+  {
+    "revision": "bd2cbde5271f1b8ce219",
+    "url": "/MakeTheBiggerSum/static/js/runtime-main.73d87d5a.js"
+  }
+]);
